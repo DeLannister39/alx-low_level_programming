786a8667f0c6f8468c90fa42825e3800e2992d17
DeLannister39 <emmanuel.atanu@techie.com>
2023-02-16 11:49:48 -0800

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a C program that prints the size of various types on the computer it is compiled and run on
+ * main - Entry
  * Return: 0(succes)
  */
 int main(void)
