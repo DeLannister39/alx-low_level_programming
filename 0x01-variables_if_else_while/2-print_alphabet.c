c6a6bdb798c3141ead60fcfc6c484538d8f0713e
DeLannister39 <emmanuel.atanu@techie.com>
2023-02-17 15:59:02 -0800

    Task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet lowercase.
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char abc[26] = "abcdefjhijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(abc[i]);
+	}
+	putchar('\n');
+	return (0);
+}
