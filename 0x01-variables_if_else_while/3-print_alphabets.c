518f12959d13748640e0513381eb5a9d9603bba7
DeLannister39 <emmanuel.atanu@techie.com>
2023-02-17 16:23:45 -0800

    task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - I swear im tired
+ *
+ * Return: 0 ()
+ *
+ */
+int main(void)
+{
+	/*Betty pls relax*/
+	char e;
+	char l;
+
+	e = 'a';
+	l = 'A';
+	while
+		(e <= 'z')
+		{
+			putchar(e);
+			e++;
+		}
+	while
+		(l <= 'Z')
+		{
+			putchar(l);
+			l++;
+		}
+	putchar('\n');
+	return (0);
+}
