b67b4aee4fc209d3ac6c75dd29e5e588478498ee
DeLannister39 <emmanuel.atanu@techie.com>
2023-02-17 17:18:30 -0800

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit
+ *
+ * Return: 0 ()
+ *
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; a < 10; a++)
+		printf("%d", d);
+	putchar('\n');
+	return (0);
+}
