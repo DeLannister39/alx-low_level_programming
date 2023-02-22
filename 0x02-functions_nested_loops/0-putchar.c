4bcead1202ede3f5b2bb2320d26959b58273a4f6
DeLannister39 <emmanuel.atanu@techie.com>
2023-02-22 01:16:41 -0800

    Task 0 correct  wahala

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,20 @@
 /**
  * main - mehn this task wasn't easy o
  *
- * Return Always 0
+ * Return: 0 (Success)
  *
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
