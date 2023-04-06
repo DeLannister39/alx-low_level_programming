93507f4f03959bd87ca8161c4c8efb750b5e1c3b
DeLannister39 <emmanuel.atanu@techie.com>
2023-04-06 12:47:21 -0700

    Task 0-read_textfile.c commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
 /**
- *
  * read_textfile - Read Text file and prints it to the POSIX
  * @letters: No of Letters to be read
  * @filename: The text file that is being read
- * Return: (0) if Filename is NULL
- * 	    w- when the actual number of bytes read
+ * Return: (0) if Filename is NULL, w- when the actual number of bytes read
  *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
-		return 0;
+		return (0);
 	}
 
 	if (filename == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
-	char *buffer = malloc(sizeof(char) * (letters + 1));
-	ssize_t bytes_read = read(fd, buffer, letters);
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	char *buffer;
+	ssize_t d;
+	ssize_t e;
+
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	d = read(fd, buffer, letters);
+	e = write(STDOUT_FILENO, buffer, d);
 
 	free(buffer);
 	close(fd);
 
-	return bytes)written;
+	return (e);
 }
 
