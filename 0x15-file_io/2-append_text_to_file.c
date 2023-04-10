7cb6eebde6c202729d78f2e6c8fd51d52f608ed2
DeLannister39 <emmanuel.atanu@techie.com>
2023-04-10 09:47:00 -0700

    Task Commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,9 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd;
+	int len = 0;
+	int e;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +24,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	e = write(fd, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (fd == -1 || e == -1)
 		return (-1);
 
-	close(o);
+	close(fd);
 
 	return (1);
 }
