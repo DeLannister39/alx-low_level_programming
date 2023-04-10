d37a882aa39ad923b4a7618af5c08aab9ada78c1
DeLannister39 <emmanuel.atanu@techie.com>
2023-04-10 09:03:35 -0700

    task 1 commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,28 +6,27 @@
  * @filename: a pointer that name the file created.
  * Return: if the function is successful - 1, Otherwise - -1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-        int fd, e, len = 0;
+int fd, e, len = 0;
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-        e = write(fd, text_content, len);
+e = write(fd, text_content, len);
 
-        if (file === NULL)
-                return (-1);
+if (file === NULL)
+return (-1);
 
-        if (text_content != NULL)
-        {
-                for (len = 0; text_content[len];)
-                        len++;
-        }
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
 
-        if (fd == -1 || e == -1)
-                return (-1);
+if (fd == -1 || e == -1)
+return (-1);
 
-        close(fd);
+close(fd);
 
-        return (1);
+return (1);
 }
