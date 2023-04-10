4eb52f76737889d30436750d9b7cfa72f836f21c
DeLannister39 <emmanuel.atanu@techie.com>
2023-04-10 08:19:09 -0700

    checking  rask 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,23 +4,32 @@
  * read_textfile - Read Text file and prints it to the POSIX
  * @letters: No of Letters to be read
  * @filename: The text file that is being read
- * Return: (0) if Filename is NULL, w- when the actual number of bytes read
+ * Return: (0) if Filename is NULL, e- when the actual number of bytes read
+ *
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
 	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	char *buffer;
+	ssize_t d;
+	ssize_t e;
+
+
+	buffer = malloc(sizeof(char) * (letters));
+	d = read(fd, buffer, letters);
+	e = write(STDOUT_FILENO, buffer, d);
+
+	free(buffer);
 	close(fd);
-	return (w);
+
+	return (e);
 }
