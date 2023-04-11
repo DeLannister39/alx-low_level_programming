a87d0223e793363c54cf688c7a41aaf51e7547ca
DeLannister39 <emmanuel.atanu@techie.com>
2023-04-11 08:29:33 -0700

    task 1 commits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,24 +7,24 @@
  */
 void print_binary(unsigned long int n)
 {
-        int a;
-        int num;
-        num = 0;
+	int a;
+	int num;
+	num = 0;
 
-        unsigned long int value;
+	unsigned long int value;
+	
+	for (a = 63; a >= 0; a--)
+	{
+		value = n >> a;
 
-        for (a = 63; a >= 0; a--)
-        {
-                value = n >> a;
-
-                if (value & 1)
-                {
-                        _putchar('1');
-                        num++;
-                }
-                else if (num)
-                        _putchar('0');
-        }
-        if (!num)
-                _putchar('0');
+		if (value & 1)
+		{
+			_putchar('1');
+			num++;
+		}
+		else if (num)
+			_putchar('0');
+	}
+	if (!num)
+		_putchar('0');
 }
